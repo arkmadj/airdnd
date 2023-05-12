7cc11a91a40852c17ba6d4622128d225f17468b5
arkmadj <ahmadjinadu@gmail.com>
2023-05-12 06:09:35 +0100

    added logo

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Container from "../Container";
+import Logo from "./Logo";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
 	return <div className="fixed z-10 w-full bg-white shadow-sm">
-    <div className="py-4 border-b-[1px]"></div>
+    <div className="py-4 border-b-[1px]">
+      <Container>
+        <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+          <Logo/>
+        </div>
+      </Container>
+    </div>
   </div>;
 };
 
