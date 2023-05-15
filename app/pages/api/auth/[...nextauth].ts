a177ab3c90090744fd67fc871d38322286b23f18
arkmadj <ahmadjinadu@gmail.com>
2023-05-15 23:02:53 +0100

    fixed typo

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ export const authOptions: AuthOptions = {
         });
 
         if (!user || !user?.hashedPassword) {
-          throw new Error('Invalid scredsednsials');
+          throw new Error('Invalid scredsensials');
         }
 
         const isCorrectPassword = await bcrypt.compare(
