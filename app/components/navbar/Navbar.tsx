6831566658980cf3baf5606bfb120cdbaadac7f3
arkmadj <ahmadjinadu@gmail.com>
2023-05-17 06:52:54 +0100

    added current user

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,25 +1,29 @@
-'use client'
+"use client";
 
-import React from "react";
+import { AirUser } from "@prisma/client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-type Props = {};
+interface NavbarProps {
+	currentUser?: AirUser | null;
+}
 
-const Navbar = (props: Props) => {
-	return <div className="fixed z-10 w-full bg-white shadow-sm">
-    <div className="py-4 border-b-[1px]">
-      <Container>
-        <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-          <Logo/>
-          <Search/>
-          <UserMenu/>
-        </div>
-      </Container>
-    </div>
-  </div>;
+const Navbar: React.FC<NavbarProps> = ({currentUser}) => {
+	return (
+		<div className="fixed z-10 w-full bg-white shadow-sm">
+			<div className="py-4 border-b-[1px]">
+				<Container>
+					<div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+						<Logo />
+						<Search />
+						<UserMenu />
+					</div>
+				</Container>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
