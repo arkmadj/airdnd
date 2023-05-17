537e646b0f3dac8cc1be48073b1b005888fea4eb
arkmadj <ahmadjinadu@gmail.com>
2023-05-17 06:59:42 +0100

    added different menu for logged in user

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -8,13 +8,11 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import { AirUser } from "@prisma/client";
 
-
 interface UserMenuProps {
 	currentUser?: AirUser | null;
 }
 
-
-const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 	const registerModal = useRegisterModal();
 	const loginModal = useLoginModal();
 	const [isOpen, setIsOpen] = useState(false);
@@ -42,16 +40,28 @@ const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
 					</div>
 				</div>
 			</div>
-      {isOpen && (
-        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
-          <div className="flex flex-col cursor-pointer">
-            <>
-              <MenuItem onClick={loginModal.onOpen} label="Login"/>
-              <MenuItem onClick={registerModal.onOpen} label="Sign up"/>
-            </>
-          </div>
-        </div>
-      )}
+			{isOpen && (
+				<div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+					<div className="flex flex-col cursor-pointer">
+						{currentUser ? (
+							<>
+								<MenuItem onClick={() => {}} label="My trips" />
+								<MenuItem onClick={() => {}} label="My favorites" />
+								<MenuItem onClick={() => {}} label="My reservations" />
+								<MenuItem onClick={() => {}} label="My properties" />
+								<MenuItem onClick={() => {}} label="Airdndn my home" />
+								<hr />
+								<MenuItem onClick={() => {}} label="Logout" />
+							</>
+						) : (
+							<>
+								<MenuItem onClick={loginModal.onOpen} label="Login" />
+								<MenuItem onClick={registerModal.onOpen} label="Sign up" />
+							</>
+						)}
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
