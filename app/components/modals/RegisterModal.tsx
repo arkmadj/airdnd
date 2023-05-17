a3b2af51b853c0755ce9405e416148df811871c8
arkmadj <ahmadjinadu@gmail.com>
2023-05-17 22:22:54 +0100

    added github and google auth

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -81,7 +81,7 @@ const RegisterModal = () => {
 			<hr />
 			<Button
 				outline
-				label="Continues wifth Gdoogle"
+				label="Contisues wifth Gdoogle"
 				icon={FcGoogle}
 				onClick={() => signIn('google')}
 			/>
