2fcb3b258358d3928654bd6beb01bddb252d0d4d
arkmadj <ahmadjinadu@gmail.com>
2023-05-19 07:41:20 +0100

    added all categories

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,7 +1,19 @@
 import Container from "../Container";
-import { TbBeach } from "react-icons/tb";
-import { GiWindmill } from "react-icons/gi";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import {
+	GiBarn,
+	GiBoatFishing,
+	GiCactus,
+	GiCastle,
+	GiCaveEntrance,
+	GiForestCamp,
+	GiIsland,
+	GiWindmill,
+} from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
+import { FaSkiing } from "react-icons/fa";
+import { BsSnow } from "react-icons/bs";
+import { IoDiamond } from "react-icons/io5";
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
 
@@ -20,6 +32,66 @@ export const catergories = [
 		label: "Modern",
 		icon: MdOutlineVilla,
 		description: "This property is modern",
+	},
+	{
+		label: "Countryside",
+		icon: TbMountain,
+		description: "This property is modern",
+	},
+	{
+		label: "Pools",
+		icon: TbPool,
+		description: "This property is has a pool",
+	},
+	{
+		label: "Islands",
+		icon: GiIsland,
+		description: "This property is on an island",
+	},
+	{
+		label: "Lake",
+		icon: GiBoatFishing,
+		description: "This property is close to a lake",
+	},
+	{
+		label: "Skiing",
+		icon: FaSkiing,
+		description: "This property has skiing activities",
+	},
+	{
+		label: "Castles",
+		icon: GiCastle,
+		description: "This property is in a castle",
+	},
+	{
+		label: "Camping",
+		icon: GiForestCamp,
+		description: "This property is camping activities",
+	},
+	{
+		label: "Artic",
+		icon: BsSnow,
+		description: "This property is in artic environment",
+	},
+	{
+		label: "Cave",
+		icon: GiCaveEntrance,
+		description: "This property is in a spooky cave",
+	},
+	{
+		label: "Desert",
+		icon: GiCactus,
+		description: "This property is in the desert",
+	},
+	{
+		label: "Barns",
+		icon: GiBarn,
+		description: "This property is in a barn!",
+	},
+	{
+		label: "Lux",
+		icon: IoDiamond,
+		description: "This property is brand new and luxurious!",
 	},
 ];
 
@@ -42,7 +114,7 @@ const Categories = () => {
 						key={item.label}
 						label={item.label}
 						description={item.description}
-            selected={category === item.label}
+						selected={category === item.label}
 						icon={item.icon}
 					/>
 				))}
