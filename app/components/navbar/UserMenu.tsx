757c560fbf04bbf4b049f8a30d8ec57ca90882fa
arkmadj <ahmadjinadu@gmail.com>
2023-05-19 08:15:30 +0100

    added rent modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -22,11 +22,19 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 		setIsOpen((value) => !value);
 	}, []);
 
+	const onRent = useCallback(() => {
+		if(!currentUser){
+			return loginModal.onOpen()
+		}
+
+
+	}, [currentUser, loginModal])
+
 	return (
 		<div className="relative">
 			<div className="flex flex-row items-center gap-3">
 				<div
-					onClick={() => {}}
+					onClick={onRent}
 					className="hidden px-4 py-3 text-sm transition rounded-full cursor-pointer md:block hover:bg-neutral-100"
 				>
 					Airdnd your home
