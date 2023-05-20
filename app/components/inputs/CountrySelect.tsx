969fa5c384946e194ec3ca19470483985451b323
arkmadj <ahmadjinadu@gmail.com>
2023-05-21 00:09:59 +0100

    reduce region darkness

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -32,7 +32,7 @@ const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
 						<div>{option.flag}</div>
 						<div>
 							{option.label},{" "}
-							<span className="ml-1 text-neutral-800">{option.region}</span>
+							<span className="ml-1 text-neutral-500">{option.region}</span>
 						</div>
 					</div>
 				)}
