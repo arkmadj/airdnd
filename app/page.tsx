1236463ae9f209e7e82e436f6bb6264a611ab313
arkmadj <ahmadjinadu@gmail.com>
2023-05-25 22:08:03 +0100

    added listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
 
 export default function Home() {
-  return (
-    <div className="text-2xl text-rose-500">Hello Airdnd!</div>
-  )
+	return (
+		<ClientOnly>
+			<Container>
+				<div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <div>My future listings</div>
+        </div>
+			</Container>
+		</ClientOnly>
+	);
 }
