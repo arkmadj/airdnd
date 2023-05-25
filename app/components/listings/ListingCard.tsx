b42b40533104bf3431e583fb532d9d46a24e4d90
arkmadj <ahmadjinadu@gmail.com>
2023-05-25 23:31:02 +0100

    added listing card info

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
@@ -8,6 +8,7 @@ import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
 	data: Listing;
@@ -75,12 +76,25 @@ const ListingCard: React.FC<ListingCardProps> = ({
 						alt="Listing"
 						src={data.imageSrc}
 						className="object-cover w-full h-full transition group-hover:scale-110"
-            fill
+						fill
 					/>
-          <div className="absolute top-3 right-3">
-            <HeartButton listingId={data.id} currentUser={currentUser}/>
-          </div>
+					<div className="absolute top-3 right-3">
+						<HeartButton listingId={data.id} currentUser={currentUser} />
+					</div>
 				</div>
+				<div className="text-lg font-semibold">
+					{location?.region}, {location?.label}
+				</div>
+				<div className="font-light text-neutral-500">
+					{reservationDate || data.category}
+				</div>
+				<div className="flex flex-row items-center gap-1">
+					<div className="font-semibold">$ {price}</div>
+					{!reservation && <div className="font-light">night</div>}
+				</div>
+				{onAction && actionLabel && (
+					<Button disabled={disabled} small label={actionLabel} onClick={handleCancel} />
+				)}
 			</div>
 		</div>
 	);
