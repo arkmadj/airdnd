57c4d16d91df9516e6078de62dfb6d3fb88ce74e
arkmadj <ahmadjinadu@gmail.com>
2023-05-29 22:55:00 +0100

    added listing client

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,7 @@
+const ListingCLient = () => {
+  return ( 
+    <div>Listing Client</div>
+   );
+}
+ 
+export default ListingCLient;
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,6 +1,8 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
+import ListingCLient from "./ListingClient";
 
 interface IParams {
 	listingId?: string;
@@ -8,15 +10,20 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
 	const listing = await getListingById(params);
+	const currentUser = await getCurrentUser();
 
-  if(!listing){
-    return (
-      <ClientOnly>
-        <EmptyState/>
-      </ClientOnly>
-    )
-  }
-	return <div>{listing?.title}</div>;
+	if (!listing) {
+		return (
+			<ClientOnly>
+				<EmptyState />
+			</ClientOnly>
+		);
+	}
+	return (
+		<ClientOnly>
+			<ListingCLient listing={listing} currentUser={currentUser}/>
+		</ClientOnly>
+	);
 };
 
 export default ListingPage;
