15a530cc1b7ec1836ecda86a52cc1fd6669b9521
arkmadj <ahmadjinadu@gmail.com>
2023-05-29 23:36:12 +0100

    added interface

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,3 +1,22 @@
+'use client'
+
+import { SafeUser } from "@/app/types";
+import { IconType } from "react-icons";
+
+interface ListingInfoProps {
+  user: SafeUser;
+  description: string;
+  guestCount: number;
+  roomCount: number;
+  bathroomCount: number;
+  category: {
+    icon: IconType;
+    label: string;
+    description: string;
+  } | undefined;
+  locationValue: string;
+}
+
 const ListingInfo = () => {
   return ( 
     <div>
