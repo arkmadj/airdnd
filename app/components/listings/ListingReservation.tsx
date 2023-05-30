f970cfaff3e5d3b4a12b80d0be3e5d2054786ca2
arkmadj <ahmadjinadu@gmail.com>
2023-05-30 13:21:46 +0100

    added react-date-range

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,8 +1,20 @@
 'use client'
 
+import { Range } from "react-date-range";
+
+interface ListingReservationProps {
+  price: number;
+  dateRange: Range;
+  totalPrice: number;
+  onChangeDate: (value: Range) => void
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[]
+}
+
 const ListingReservation = () => {
   return ( 
-    <div>Listing Reservat</div>
+    <div></div>
    );
 }
  
