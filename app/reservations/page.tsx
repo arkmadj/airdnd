5a54fb73c3dc86b32e99949acacfe37f5d92157c
arkmadj <ahmadjinadu@gmail.com>
2023-05-31 07:38:11 +0100

    added empty reservations state

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -4,17 +4,32 @@ import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
 import getReservations from "../actions/getReservations";
 
-const ReservationPage = async() => {
-  const currentUser = await getCurrentUser()
+const ReservationPage = async () => {
+	const currentUser = await getCurrentUser();
 
-  if(!currentUser){
-    return (
-      <ClientOnly>
-        <EmptyState title="Unauthorized" subtitle="Please login"/>
-      </ClientOnly>
-    )
-  }
-  return (  );
-}
- 
+	if (!currentUser) {
+		return (
+			<ClientOnly>
+				<EmptyState title="Unauthorized" subtitle="Please login" />
+			</ClientOnly>
+		);
+	}
+
+	const reservations = await getReservations({
+		authorId: currentUser.id,
+	});
+
+	if (reservations.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="No reservations found"
+					subtitle="Looks like you have no reservations on your property"
+				/>
+			</ClientOnly>
+		);
+	}
+	return <div>Reservations</div>;
+};
+
 export default ReservationPage;
