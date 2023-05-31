b7750289a5db42c2f8fcf03bde650281ae5e4f16
arkmadj <ahmadjinadu@gmail.com>
2023-05-31 11:27:50 +0100

    added favorites card

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
 import { SafeListing, SafeUser } from "../types";
 
 interface FavoriteClientProps {
@@ -11,7 +14,20 @@ const FavoritesClient: React.FC<FavoriteClientProps> = ({
 	listings,
 	currentUser,
 }) => {
-	return <div>Favorites Client</div>;
+	return (
+		<Container>
+			<Heading title="Favorites" subtitle="List of places you have favorited" />
+			<div className="grid grid-cols-1 gap-8 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+				{listings.map((listing) => (
+					<ListingCard
+						currentUser={currentUser}
+						key={listing.id}
+						data={listing}
+					/>
+				))}
+			</div>
+		</Container>
+	);
 };
 
 export default FavoritesClient;
