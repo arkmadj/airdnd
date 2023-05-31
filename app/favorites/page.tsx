3a79c51dffc98e3fadd178ea959fdb749474cc71
arkmadj <ahmadjinadu@gmail.com>
2023-05-31 11:12:42 +0100

    added getFavorites

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,6 +1,7 @@
 import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
 
 const FavoritesPage = async () => {
 	return (
