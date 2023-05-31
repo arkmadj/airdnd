9c328249bdef78e9670ee9d0835b1f77a1471a26
arkmadj <ahmadjinadu@gmail.com>
2023-05-31 07:47:58 +0100

    added reservationsClientprops interface

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,11 +1,23 @@
-'use client'
+"use client";
 
-const ReservationsClient = () => {
-  return ( 
-    <div>
-      Reservation Client
-    </div>
-   );
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import { SafeReservations, SafeUser } from "../types";
+
+interface ReservationsClientProps {
+	reservations: SafeReservations[];
+	currentUser?: SafeUser | null;
 }
- 
+
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+	reservations,
+	currentUser,
+}) => {
+	return (
+		<Container>
+			<Heading title="Reservations" subtitle="Bookings on your properties" />
+		</Container>
+	);
+};
+
 export default ReservationsClient;
