e8f9b58b25a04ce404fe86b66783b2a39f25799a
arkmadj <ahmadjinadu@gmail.com>
2023-06-01 08:11:00 +0100

    added more info content

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -12,6 +12,7 @@ import qs from "query-string";
 import { formatISO } from "date-fns";
 import Heading from "../Heading";
 import Calendar from "../inputs/Calendar";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
 	LOCATION = 0,
@@ -149,6 +150,32 @@ const SearchModal = () => {
 		);
 	}
 
+	if (step === STEPS.INFO) {
+		bodyContent = (
+			<div className="flex flex-col gap-8">
+				<Heading title="More information" subtitle="Find your perfect place" />
+				<Counter
+					title="Guests"
+					subtitle="How many guests are coming?"
+					value={guestCount}
+					onChange={(value) => setGuestCount(value)}
+				/>
+				<Counter
+					title="Rooms"
+					subtitle="How many rooms do you need?"
+					value={roomCount}
+					onChange={(value) => setRoomCount(value)}
+				/>
+				<Counter
+					title="Bathrooms"
+					subtitle="How many bathrooms do you need?"
+					value={bathroomCount}
+					onChange={(value) => setBathroomCount(value)}
+				/>
+			</div>
+		);
+	}
+
 	return (
 		<Modal
 			isOpen={searchModal.isOpen}
@@ -156,6 +183,8 @@ const SearchModal = () => {
 			onSubmit={onSubmit}
 			title="Filters"
 			actionLabel={actionLabel}
+			secondaryActionLabel={secondaryActionLabel}
+			secondaryAction={step === STEPS.LOCATION ? undefined : onBack}
 			body={bodyContent}
 		/>
 	);
