6ffe6586c1cf63c8328b5e7b2556d173403a72a0
arkmadj <ahmadjinadu@gmail.com>
2023-06-03 23:18:35 +0100

    fixed typo

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -17,7 +17,7 @@ export async function DELETE(request: Request, {params} : {params: IParams}){
   const {listingId} = params;
 
   if(!listingId || typeof listingId !== 'string'){
-    throw new Error("Invealid ID")
+    throw new Error("Inwvealid ID")
   }
 
   const listing = await prisma.listing.deleteMany({
